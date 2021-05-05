916bbbef6d1425c93310ee8eb07e2e3a14ff9650
sarabizjak7 <sarabizjak97@gmail.com>
2021-05-05 14:31:02 +0200

    Dodala mapo za DN4.

---
+++ HW/HW4/test.py
@@ -0,0 +1,45 @@
+def modularnoPotenciranje(a, b, n):
+    """
+    Vhod: a, b, n
+    Izhod: a^b mod n
+    """
+
+    if b == 0:
+        return 1
+
+    d = 1 # za mnozenja pri lihih bitih
+    # osnova po mod n, ki jo na vsakem koraku kvadriramo
+    osnova = a % n       
+    while b > 1:
+        # trenutni bit liho stevilo
+        if b % 2 == 1:       
+            d = (osnova * d) % n
+        # trenutni bit sodo stevilo
+        else:
+            b = b // 2
+            osnova = (osnova ** 2) % n
+
+    osnova = (osnova * d) % n
+    return osnova
+
+
+#( TESTI )#
+"""
+a1 = 106
+b1 = 178
+n1 = 908
+print(modularnoPotenciranje(a1, b1, n1))
+print((a1 ** b1) % n1)
+
+a2 = 1063
+b2 = 128
+n2 = 1819
+print(modularnoPotenciranje(a2, b2, n2))
+print((a2 ** b2) % n2)
+
+a3 = 106
+b3 = 0
+n3 = 908
+print(modularnoPotenciranje(a3, b3, n3))
+print((a3 ** b3) % n3)
+"""
